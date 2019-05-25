e101ec547e862d4b70e09f3139c089daedc35323
Kunal Vohra <vohra.kunal3@gmail.com>
2019-05-25 21:01:12 +0530

    Move Projects Outside

---
+++ out/production/Learning/kunal/Test/README.md
@@ -0,0 +1,4 @@
+# Coding-Challenge
+This is the repo for JAVA basic coding questions
+# It is preferred to use intellij for these solutions.
+On other IDEs it may give compiilation error as there is mismatch of file name and class name.
--- src/kunal/Test/PalindromePermuntation.java
+++ src/kunal/Test/PalindromePermuntation.java
@@ -4,34 +4,31 @@ package kunal.Test;
  */
 
 
-import java.io.*;
-import java.util.*;
-import java.math.*;
+import static kunal.Test.Working_Palindrome2.checkForPalindrome;
 
 public class PalindromePermuntation {
-        static int NO_OF_CHARS = 256;
+    static int NO_OF_CHARS = 256;
 
-        /* function to check whether characters
-        of a string can form a palindrome */
-        static boolean canFormPalindrome(String str) {
+    /* function to check whether characters
+    of a string can form a palindrome */
+    static boolean canFormPalindrome(String str) {
 
-            // Create a count array and initialize all
-            // values as 0
-            int count[] = new int[NO_OF_CHARS];
-            return checkForPalindrome(str, count, NO_OF_CHARS);
-        }
-
-        // Driver program
-        public static void main(String args[])
-        {
-            if (canFormPalindrome("geeksforgeeks"))
-                System.out.println("Yes");
-            else
-                System.out.println("No");
-
-            if (canFormPalindrome("geeksogeeks"))
-                System.out.println("Yes");
-            else
-                System.out.println("No");
-        }
+        // Create a count array and initialize all
+        // values as 0
+        int count[] = new int[NO_OF_CHARS];
+        return checkForPalindrome(str, count, NO_OF_CHARS);
     }
+
+    // Driver program
+    public static void main(String args[]) {
+        if (canFormPalindrome("geeksforgeeks"))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+
+        if (canFormPalindrome("geeksogeeks"))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
