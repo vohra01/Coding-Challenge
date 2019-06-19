30cfd6142acfe708935f46387438e6e5ad765368
vohra01 <38027302+vohra01@users.noreply.github.com>
2019-06-19 21:10:41 +0530

    Dictionary Word Problem with extended cases (#1)

---
+++ src/kunal/Main/HashSetCollection.java
@@ -0,0 +1,22 @@
+package kunal.Main;
+
+import java.util.HashSet;
+
+import static java.util.Arrays.asList;
+
+public class HashSetCollection {
+
+    public static void main(String[] args) {
+
+        HashSet<String> hashSet = new HashSet<>(asList());
+
+        HashSet hashSet1 = new HashSet();
+
+        float f = 3.14f;
+        hashSet.add("");
+        hashSet1.add(1);
+
+    }
+
+
+}
